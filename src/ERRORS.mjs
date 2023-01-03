25b0e0dded5bb8ddb3d80cf36c794fe8b2020151
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-01-03 15:43:23 +0530

    Added: Error Handling, Support for CommonJS, ESM & TS

---
+++ src/ERRORS.mjs
@@ -0,0 +1,17 @@
+export const INVALID_INTERPOLATE_XML_ARGS_ERROR = {
+  message: 'Invalid interpolateXml Arguments: templatePath cannot be blank',
+  statusCode: 500,
+  errorCode: 'XmlUtils::INVALID_INTERPOLATE_XML_ARGS'
+}
+
+export const INVALID_XML_TO_JSON_XML_ARGS_ERROR = {
+  message: 'Invalid xmlToJSON Arguments: xml cannot be blank',
+  statusCode: 500,
+  errorCode: 'XmlUtils::INVALID_XML_TO_JSON_ARGS'
+}
+
+export const INVALID_XML_TO_JSON_XPATH_ARGS_ERROR = {
+  message: 'Invalid xmlToJSON Arguments: xPathTemplate cannot be blank',
+  statusCode: 500,
+  errorCode: 'XmlUtils::INVALID_XML_TO_JSON_ARGS'
+}
--- src/XmlError.mjs
+++ src/XmlError.mjs
@@ -1,17 +1,38 @@
+const DEFAULT_ERROR_MSG = 'Xml Utils Error'
+const DEFAULT_ERROR_STATUS_CODE = 500
+const DEFAULT_ERROR_CODE = 'XML_UTILS_ERROR'
+
 export default class XmlError extends Error {
-  constructor (message, error) {
-    super(message)
+  constructor (e = {}, eMap) {
+    if (e._isCustomError && !eMap) { return e }
 
+    super()
+
+    const { message, statusCode, errorCode } = eMap || {}
+    const {
+      message: eMessage,
+      msg: eMsg,
+      statusCode: eStatusCode,
+      errorCode: eErrorCode,
+      code: eCode
+    } = e
+
+    const {
+      npm_package_name: pkgName = '',
+      npm_package_version: pkgVersion = ''
+    } = process.env
+    const service = `${pkgName}@${pkgVersion}`
+
+    this._isCustomError = true
     this._isXmlError = true
-    this.message = message
-    this.statusCode = 500
-    this.error = error
-
-    this.toJSON = this.toJSON.bind(this)
-  }
-
-  toJSON () {
-    const { message, statusCode, error } = this
-    return { message, statusCode, error }
+    this.service = service
+    this.message = message || eMessage || eMsg || DEFAULT_ERROR_MSG
+    this.statusCode = statusCode || eStatusCode || DEFAULT_ERROR_STATUS_CODE
+    this.errorCode = errorCode || eErrorCode || eCode || DEFAULT_ERROR_CODE
+    this.error = {
+      ...e,
+      message: eMessage || this.message,
+      errorCode: eErrorCode || this.errorCode
+    }
   }
 }
